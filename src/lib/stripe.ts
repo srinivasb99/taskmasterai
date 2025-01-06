49976cb2159d1ac348eaf969bc770a925c67ba88
taskmasteroneai <taskmasteroneai@gmail.com>
2025-01-05 19:00:45 -0600

    Create stripe.ts

---
+++ src/lib/stripe.ts
@@ -0,0 +1,3 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+export const stripePromise = loadStripe('pk_live_51QdhwgIdgEonJvEbYZYASetFGNVWiSHN68N3FtLpybd4QIhEjfXQxk3XJoxhEDor4gaT7hZxjN4HSDAz1fAQiuFV00Q4nb1AlL');

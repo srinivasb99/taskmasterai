74716e39630c9acaec42e55d57887c5904f8c94e
taskmasteroneai <taskmasteroneai@gmail.com>
2025-01-05 19:45:34 -0600

    Delete server directory

--- server/middleware/cors.js
+++
@@ -1,7 +0,0 @@
-import cors from 'cors';
-
-export const corsMiddleware = cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-});

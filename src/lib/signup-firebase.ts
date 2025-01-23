7965c94944c2052e88d6d9763dad48f2557b439c
srini <srinibaj10@gmail.com>
2025-01-23 14:49:31 -0500

    Update signup-firebase.ts

--- src/lib/signup-firebase.ts
+++ src/lib/signup-firebase.ts
@@ -3,13 +3,13 @@ import { auth } from './firebase';
 
 export const emailSignUp = async (email: string, password: string) => {
   const result = await createUserWithEmailAndPassword(auth, email, password);
-  window.location.href = '/splashscreen.html';
+  window.location.href = '/splashscreen';
   return result;
 };
 
 export const googleSignUp = async () => {
   const provider = new GoogleAuthProvider();
   const result = await signInWithPopup(auth, provider);
-  window.location.href = '/splashscreen.html';
+  window.location.href = '/splashscreen';
   return result;
 };
